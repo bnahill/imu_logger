8b101fdfcda21a398b9952df53b2a989d0099a03
Ben Nahill <bnahill@gmail.com>
2012-05-27 22:34:14 -0400

    Added more robust logging structure -- still untested
    Change-Id: I3eb3f52e17f11b170c8d12f3715c5d1ad61ee128

--- logger/sw/src/logger.c
+++ logger/sw/src/logger.c
@@ -4,37 +4,45 @@
 #include "lps001.h"
 #include "ff.h"
 
-#define LOG_BUFFER_LEN 8192
+#include "log_format.h"
 
-typedef struct {
-	float gyro_pitch;
-	float gyro_roll;
-	float gyro_yaw;
-	float acc_x;
-	float acc_y;
-	float acc_z;
-	float mag_x;
-	float mag_y;
-	float mag_z;
-	float pressure;
-} log_frame_t;
 
-// sizeof can't be used in preprocessor conditionals...
-#define LOG_FRAME_SIZE (4 * 10)
+static log_config_header_t log_header = {
+	LOG_KEY,
+	LOG_VERSION,
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	LOG_PAGE_LEN,
+	50,
+	{
+		1, // Gyro 1;
+		0, // Gyro 2
+		1, // Acc 1
+		0, // Acc 2
+		2, // Mag 1
+		0, // Mag 2
+		4, // Pressure 1
+		0, // Pressure 2
+		0, // Temperature 1
+		0 // Temperature 2
+	}
+};
 
-#define LOG_BUFFER_FRAMES ((LOG_BUFFER_LEN - 4) / LOG_FRAME_SIZE)
-#define LOG_BUFFER_PADDING ((LOG_BUFFER_LEN - 4) - (LOG_BUFFER_FRAMES * LOG_FRAME_SIZE))
+//! A buffer to contain the current page to write to the log
+static uint32_t write_buffer[LOG_PAGE_LEN >> 2];
 
-static struct {
-	uint32_t header;
-	log_frame_t frames[LOG_BUFFER_FRAMES];
-#if LOG_BUFFER_PADDING > 0
-	uint32_t padding[LOG_BUFFER_PADDING / 4];
-#endif
-} write_buffer;
+//! Pointer to the current page header in write_buffer
+static uint32_t *frameset_header;
 
-static log_frame_t *write_ptr;
+//! Number of cycles so far in the current frameset
+static uint32_t cycle_count;
 
+//! The data (not header) write head within write_buffer
+static union {
+	uint32_t  *u;
+	float     *f;
+} write_ptr;
+
+static UINT write_len;
 static char filename[32];
 static FATFS fs;
 static FIL file;
@@ -44,6 +52,8 @@ const char *logger_init(const char *prefix){
 	char *ptr, *suffix;
 	int i, j, tmp;
 	FILINFO fi;
+	const uint8_t *u_id = (uint8_t *)0x1FFFF7E8;
+	
 	if(f_mount(0, &fs) != FR_OK){
 		return NULL;
 	}
@@ -90,44 +100,132 @@ const char *logger_init(const char *prefix){
 		return NULL;
 	}
 	
-	write_ptr = write_buffer.frames;
+	log_header.device_id[11] = u_id[1];
+	log_header.device_id[10] = u_id[0];
+	log_header.device_id[9] = u_id[3];
+	log_header.device_id[8] = u_id[2];
+	log_header.device_id[7] = u_id[7];
+	log_header.device_id[6] = u_id[6];
+	log_header.device_id[5] = u_id[5];
+	log_header.device_id[4] = u_id[4];
+	log_header.device_id[3] = u_id[11];
+	log_header.device_id[2] = u_id[10];
+	log_header.device_id[1] = u_id[9];
+	log_header.device_id[0] = u_id[8];
+	
+	// Seek to pre-allocate
+	if(f_lseek(&file, LOG_PAGE_LEN) != FR_OK)
+		return NULL;
+	
+	// Write header
+	if(f_write(&file, (uint8_t *)&log_header, sizeof(log_header), &write_len) != FR_OK)
+		return NULL;
+	
+	// Seek to end of header
+	if(f_lseek(&file, LOG_PAGE_LEN) != FR_OK)
+		return NULL;
+	
+	frameset_header = write_buffer;
+	write_ptr.u = write_buffer + 1;
+	cycle_count = 0;
 	
 	return filename;
 }
 
+static INLINE uint32_t logger_space_left(void){
+	return (&write_buffer[LOG_PAGE_LEN >> 2] - (write_ptr.u - 1)) << 2;
+}
+
+void logger_insert_silence(uint32_t duration){
+	*(frameset_header++) = duration & 0x7FFFFFFF;
+	write_ptr.u += 1;
+	cycle_count = 0;
+	if(logger_space_left() < LOG_MAX_FRAME_SIZE){
+		if(f_write(&file, (uint8_t *)&write_buffer, LOG_PAGE_LEN, &write_len) != FR_OK){
+			while(1);
+		}
+
+		// There's enough room for this -- indicate no more framesets in this page
+		*frameset_header = 0;
+			
+		write_ptr.u = write_buffer + 1;
+		frameset_header = write_buffer;
+		cycle_count = 0;
+	}
+}
+
 void logger_update(void){
-	write_ptr->gyro_pitch = gyro.reading.pitch;
-	write_ptr->gyro_roll = gyro.reading.roll;
-	write_ptr->gyro_yaw = gyro.reading.yaw;
-	write_ptr->acc_x = magacc.acc.x;
-	write_ptr->acc_y = magacc.acc.y;
-	write_ptr->acc_z = magacc.acc.z;
-	write_ptr->mag_x = magacc.mag.x;
-	write_ptr->mag_y = magacc.mag.y;
-	write_ptr->mag_z = magacc.mag.z;
-	write_ptr->pressure = pressure.pressure;
-	if(write_ptr - write_buffer.frames == LOG_BUFFER_FRAMES){
-		// The page is full, write it
-		logger_sync();
+	int i;
+	
+	for(i = 0; i < LOG_MAX_SENSORS; i++){
+		if(!log_header.sensor_sub_rate[i] || (cycle_count % log_header.sensor_sub_rate[i]))
+			continue;
+		switch(i){
+		case LOG_SENSOR_GYR1:
+			*(write_ptr.f++) = gyro.reading.pitch;
+			*(write_ptr.f++) = gyro.reading.roll;
+			*(write_ptr.f++) = gyro.reading.yaw;
+			break;
+		case LOG_SENSOR_ACC1:
+			*(write_ptr.f++) = magacc.acc.x;
+			*(write_ptr.f++) = magacc.acc.y;
+			*(write_ptr.f++) = magacc.acc.z;
+			break;
+		case LOG_SENSOR_MAG1:
+			*(write_ptr.f++) = magacc.mag.x;
+			*(write_ptr.f++) = magacc.mag.y;
+			*(write_ptr.f++) = magacc.mag.z;
+			break;
+		case LOG_SENSOR_PRESS1:
+			*(write_ptr.f++) =  pressure.pressure;
+			break;
+		case LOG_SENSOR_TEMP1:
+			*(write_ptr.f++) =  pressure.temperature;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	if(logger_space_left() < LOG_MAX_FRAME_SIZE){
+		// Not enough space left in frame
+		*frameset_header = 0x80000000 | cycle_count;
+		
+		if(write_ptr.u < &write_buffer[LOG_PAGE_LEN >> 2]){
+			// Need to indicate that the page is over
+			*(frameset_header+1) = 0;
+		}
+		
+		if(f_write(&file, (uint8_t *)&write_buffer, LOG_PAGE_LEN, &write_len) != FR_OK){
+			while(1);
+		}
+		
+		frameset_header = write_buffer;
+		write_ptr.u = write_buffer + 1;
+		cycle_count = 0;
 	} else {
-		write_ptr += 1;
+		cycle_count += 1;
 	}
 }
 
 void logger_sync(void){
-	UINT write_len;
-	// Write header section
-	write_buffer.header = (write_ptr - 1 - write_buffer.frames) & 0x7FFFFFFF;
+	if(cycle_count){
+		// We were in the middle of a frameset, finalize it
+		*(frameset_header++) = 0x80000000 | cycle_count;
+	}
 	
-	// Check if data is empty
-	if(write_buffer.header == 0)
-		return;
-		
-	if(f_write(&file, (uint8_t *)&write_buffer, LOG_BUFFER_LEN, &write_len) != FR_OK)
+	// Indicate that there are no further framesets in this page
+	*frameset_header = 0;
+	
+	// Commit the data
+	if(f_write(&file, (uint8_t *)&write_buffer, LOG_PAGE_LEN, &write_len) != FR_OK)
 		while(1);
-
-	// Reset write pointer
-	write_ptr = write_buffer.frames;
+	
+	// Reset pointers
+	frameset_header = write_buffer;
+	write_ptr.u = write_buffer + 1;
+	cycle_count = 0;
+	
 	// This doesn't really do anything but perhaps later it will
 	f_sync(&file);
 }
--- logger/sw/src/sensor_config.h
+++ logger/sw/src/sensor_config.h
@@ -1,3 +1,10 @@
+/*!
+ @file sensor_config.h
+ 
+ This file describes what sensors are connected to the platform and are to be
+ used.
+ */
+
 #ifndef __SENSOR_CONFIG_H_
 #define __SENSOR_CONFIG_H_
 
@@ -32,30 +39,6 @@
 #define HAS_PRESSURE 1
 
 //////////////////////////////////////////////////////////////////////////////
-// SD Card
-//////////////////////////////////////////////////////////////////////////////
-
-#define USE_SDIO     1
-
-//////////////////////////////////////////////////////////////////////////////
-// Doxygen-related extras
-//////////////////////////////////////////////////////////////////////////////
-
-#ifdef DOXYGEN
-	#undef HAS_STTS
-	#undef HAS_GYRO_1
-	#undef HAS_GYRO_2
-	#undef HAS_MAGACC_1
-	#undef HAS_MAGACC_2
-
-	#define HAS_STTS     1
-	#define HAS_GYRO_1   1
-	#define HAS_GYRO_2   0
-	#define HAS_MAGACC_1 1
-	#define HAS_MAGACC_2 1
-#endif
-
-//////////////////////////////////////////////////////////////////////////////
 // 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -65,10 +48,6 @@
 
 #define USE_SPI1 ((HAS_GYRO_1 || HAS_GYRO_2) ? 1 : 0)
 #define USE_SPI2 0
-
-#if USE_I2C3 && USE_SDIO
-	#error "SDIO and I2C3 cannot be used at the same time"
-#endif
 
 typedef struct {
 	GPIO_TypeDef *const gpio;
@@ -81,12 +60,14 @@ typedef struct {
 	uint16_t const pin;
 } gpio_pin_t;
 
+//! Representation of 3-dimensional Euclidean coordinates
 typedef struct {
 	float x;
 	float y;
 	float z;
 } euclidean3_t;
 
+//! Representation of 3-dimensional Euler angles
 typedef struct {
 	float pitch;
 	float roll;

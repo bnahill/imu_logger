a4fc60c034893e2f62c0f281acc884a30faedefb
Ben Nahill <bnahill@gmail.com>
2012-07-08 20:08:52 -0500

    Modified parser to support operation from web interface

--- logger/sw/parser/mkmat.py
+++ logger/sw/parser/mkmat.py
@@ -2,14 +2,16 @@ from parse import parse
 from scipy.io import savemat
 import sys
 
-if __name__ == '__main__':
-	if len(sys.argv) != 3:
-		sys.exit("Please specify an input file and an output file")
-	
-	with open(sys.argv[1], 'r') as f:
+def mkmat(infile, outfile):
+	with open(infile, 'r') as f:
 		(header, data) = parse(f)
-		print("Processed data; generating output...")
 		mat = header
 		for i in range(len(data)):
 			mat["data"+str(i)] = data[i]
-		savemat(sys.argv[2], mat, oned_as='column')
+		savemat(outfile, mat, oned_as='column')
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		sys.exit("Please specify an input file and an output file")
+	mkmat(sys.argv[1], sys.argv[2])
+
